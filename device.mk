a3e9655a60861a2f5bf53bd571c3dca28d95c385
eray orçunus <erayorcunus@gmail.com>
2019-04-27 23:32:35 +0300

    phoenix: overlay: Add 2 more notification icons
    Change-Id: I96f218b55426cb5e04e7bc661fe6179d544e97ad
    Signed-off-by: eray orçunus <erayorcunus@gmail.com>

--- device.mk
+++ device.mk
@@ -10,6 +10,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # API level, the device has been commercially launched on
 PRODUCT_SHIPPING_API_LEVEL := 29
 
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
+
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
