7b1915f8a1a7938a1e33101b3bf3314a7186359f
Pranav Vashi <neobuddy89@gmail.com>
2021-01-07 08:27:40 +0530

    phoenix: init: Workaround safetynet
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: Id1061762e4458e84c068a04756efa61e0737d9cc

--- init/init_phoenix.cpp
+++ init/init_phoenix.cpp
@@ -42,7 +42,7 @@ using android::base::GetProperty;
 
 constexpr const char *RO_PROP_SOURCES[] = {
     nullptr,   "product.", "product_services.", "odm.",
-    "vendor.", "system.",  "bootimage.",
+    "vendor.", "system.", "system_ext.", "bootimage.",
 };
 
 constexpr const char *BRANDS[] = {
@@ -57,19 +57,17 @@ constexpr const char *PRODUCTS[] = {
 
 constexpr const char *DEVICES[] = {
     "Redmi K30",
-    "POCO X2",
+    "Redmi K30",
 };
 
 constexpr const char *BUILD_DESCRIPTION[] = {
-    "phoenix-user 10 QKQ1.190825.002 V12.0.3.0.QGHCNXM release-keys",
-    "phoenixin-user 10 QKQ1.190825.002 V11.0.11.0.QGHINXM release-keys",
+    "redfin-user 11 RQ2A.210305.006 7119741 release-keys",
+    "redfin-user 11 RQ2A.210305.006 7119741 release-keys",
 };
 
 constexpr const char *BUILD_FINGERPRINT[] = {
-    "Redmi/phoenix/phoenix:10/QKQ1.190825.002/V12.0.3.0.QGHCNXM:user/"
-    "release-keys",
-    "POCO/phoenixin/phoenixin:10/QKQ1.190825.002/V11.0.11.0.QGHINXM:user/"
-    "release-keys",
+    "google/redfin/redfin:11/RQ2A.210305.006/7119741:user/release-keys",
+    "google/redfin/redfin:11/RQ2A.210305.006/7119741:user/release-keys",
 };
 
 constexpr const char *CLIENT_ID[] = {
