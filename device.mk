ec55253d296eb48b7fff233c3b3e87319321422b
Rasenkai <rasenkai99@gmail.com>
2021-03-03 07:12:53 +0000

    phoenix: Build fingerprint service
    Signed-off-by: Rasenkai <rasenkai99@gmail.com>
    Change-Id: I9d76cc7989b04e5c36e042beb24d82088d24d1b6

--- device.mk
+++ device.mk
@@ -32,6 +32,7 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sm6150 \
     libkeymaster_messages.vendor:64
 
 # Input
