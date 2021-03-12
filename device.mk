4e8e6e54ae816623d41427d7d4b6f03406b303b3
Arian <arian.kulmer@web.de>
2021-03-12 20:06:03 +0100

    phoenix: Declare shipping API level
    Change-Id: Iaa9e88cd2cd3d77ea77af16115e1ad609775b553

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# API level, the device has been commercially launched on
+PRODUCT_SHIPPING_API_LEVEL := 29
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
