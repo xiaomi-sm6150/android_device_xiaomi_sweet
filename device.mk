0532bcf0a3050088ae180101a906fbf9ce30e0b2
Arian <arian.kulmer@web.de>
2021-03-12 20:12:07 +0100

    phoenix: Configure screen density
    Change-Id: Id0ee9fd1a8c63c9a3a1778e431d9e8b0357f0538

--- device.mk
+++ device.mk
@@ -10,6 +10,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # API level, the device has been commercially launched on
 PRODUCT_SHIPPING_API_LEVEL := 29
 
+# Screen density
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+TARGET_SCREEN_DENSITY := 440
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
