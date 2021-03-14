548c3305eccf86d6c4705428c4c0fde34381a639
Arian <arian.kulmer@web.de>
2021-03-14 13:43:12 +0100

    phoenix: Add NFC support
    Change-Id: Ibdb149d6152e01382ed44302ca0ee28b56b5a53e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -15,6 +15,9 @@ TARGET_OTA_ASSERT_DEVICE := phoenix,phoenixin
 # Kernel
 TARGET_KERNEL_CONFIG := phoenix_defconfig
 
+# HIDL
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
+
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_phoenix
 TARGET_RECOVERY_DEVICE_MODULES := libinit_phoenix
