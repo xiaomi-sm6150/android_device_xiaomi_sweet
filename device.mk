8ae79d527c38de19bb49b481dd8c8d7da51170cf
Arian <arian.kulmer@web.de>
2021-03-14 13:21:16 +0100

    phoenix: Inherit from sm6150-common
    Change-Id: I49847897d0265323e50fbae467eecb70be68a990

--- device.mk
+++ device.mk
@@ -7,6 +7,9 @@
 # Installs gsi keys into ramdisk, to boot a GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
+# Inherit from sm6150-common
+$(call inherit-product, device/xiaomi/sm6150-common/sm6150.mk)
+
 # API level, the device has been commercially launched on
 PRODUCT_SHIPPING_API_LEVEL := 29
 
