d696c80a0568cb1693e6a57a6aaa593ec0472896
Arian <arian.kulmer@web.de>
2021-03-27 11:46:57 +0100

    phoenix: Add consumerir support
    Change-Id: Ic21699e059f92a7dbf883ef2359c5fcbdd40aeb7

--- device.mk
+++ device.mk
@@ -40,6 +40,14 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(LOCAL_PATH)/configs/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
+# IR
+PRODUCT_PACKAGES += \
+    android.hardware.ir@1.0-impl \
+    android.hardware.ir@1.0-service
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
+
 # NFC
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
