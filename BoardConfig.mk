fcb2d14df33024a6188ac8935fdd8d67cd93377d
Rasenkai <rasenkai99@gmail.com>
2021-04-04 15:55:44 +0530

    phoenix: Disable NFC for indian devices
    * setup phoenix sku
    
    Signed-off-by: Rasenkai <rasenkai99@gmail.com>
    Change-Id: I9f770c4a2f514c694a499b9cfa68a4e6351efbdf

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,6 +17,10 @@ TARGET_KERNEL_CONFIG := phoenix_defconfig
 
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
+ODM_MANIFEST_SKUS += phoenix
+ODM_MANIFEST_PHOENIX_FILES := \
+    $(COMMON_PATH)/configs/hidl/manifest-qva.xml \
+    $(DEVICE_PATH)/configs/hidl/manifest-nfc.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_phoenix
