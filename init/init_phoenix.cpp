a8d261418be1a62aa4d2cb14ef6e4f1773fc389a
Rasenkai <rasenkai99@gmail.com>
2021-04-08 17:50:30 +0530

    phoenix: Update Build Fingerprint to Redfin April 2021
    Signed-off-by: Rasenkai <rasenkai99@gmail.com>
    Change-Id: I21c00a8500193854ae1c04b6ef95894a4b258dbf

--- init/init_phoenix.cpp
+++ init/init_phoenix.cpp
@@ -61,13 +61,13 @@ constexpr const char *DEVICES[] = {
 };
 
 constexpr const char *BUILD_DESCRIPTION[] = {
-    "redfin-user 11 RQ2A.210305.006 7119741 release-keys",
-    "redfin-user 11 RQ2A.210305.006 7119741 release-keys",
+    "redfin-user 11 RQ2A.210405.005 7181113 release-keys",
+    "redfin-user 11 RQ2A.210405.005 7181113 release-keys",
 };
 
 constexpr const char *BUILD_FINGERPRINT[] = {
-    "google/redfin/redfin:11/RQ2A.210305.006/7119741:user/release-keys",
-    "google/redfin/redfin:11/RQ2A.210305.006/7119741:user/release-keys",
+    "google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys",
+    "google/redfin/redfin:11/RQ2A.210405.005/7181113:user/release-keys",
 };
 
 constexpr const char *CLIENT_ID[] = {
