9a45b90b9345f657155e6b20f12feb6e54e6b205
Arian <arian.kulmer@web.de>
2021-06-24 14:56:02 +0200

    sweet: Move screen density to BoardConfig
    Change-Id: I4a78934919be10c836816a139446185eab580694

--- device.mk
+++ device.mk
@@ -91,9 +91,6 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_PACKAGES += \
     fastbootd
 
-# Screen density
-TARGET_SCREEN_DENSITY := 440
-
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
