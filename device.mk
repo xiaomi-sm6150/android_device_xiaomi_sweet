7c2763047bf84d797996d6363eccb4914a4e6121
Rasenkai <rasenkai99@gmail.com>
2021-09-04 03:43:53 +0000

    sweet: s/overlay-lineage/overlay-pa
    Signed-off-by: Rasenkai <rasenkai99@gmail.com>

--- device.mk
+++ device.mk
@@ -89,7 +89,7 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-pa
 
 # Partitions
 PRODUCT_BUILD_SUPER_PARTITION := false
