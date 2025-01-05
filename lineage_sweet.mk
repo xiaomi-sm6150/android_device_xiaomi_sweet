51ed54fa387bfc7b25c057a8d943d6dde974b7a9
basamaryan <basam.aryan@gmail.com>
2025-01-05 03:54:20 -0500

    sweet: Correct fingerprint
    Change-Id: Ic6382211cb1022973f3ccb109dad116cbe3611a5

--- lineage_sweet.mk
+++ lineage_sweet.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021-2025 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -22,6 +22,6 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := Redmi/sweet_global/sweet:13/TKQ1.221013.002/V14.0.9.0.TKFMIXM:user/release-keys
+BUILD_FINGERPRINT := Redmi/sweet_global/sweet:13/RKQ1.210614.002/V14.0.9.0.TKFMIXM:user/release-keys
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="sweet_global-user 13 TKQ1.221013.002 V14.0.9.0.TKFMIXM release-keys"
+    PRIVATE_BUILD_DESC="sweet_global-user 13 RKQ1.210614.002 V14.0.9.0.TKFMIXM release-keys"
